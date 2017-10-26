76776667cd61737acb590f8a5b6eb7e4dc2465c1
Mondale Felix <mondalefelix@yahoo.com>
2017-10-26 16:50:11 -0700

    refactored code:

--- rearrange.py
+++ rearrange.py
@@ -1,5 +1,5 @@
 import sys
-import random
+import random as r
 
 
 words_list = sys.argv[1:]
@@ -7,8 +7,8 @@ words_list = sys.argv[1:]
 def mix_words():
     scrambled_words = []
     for i in words_list:
-        scrambled_words.insert(random.randint(0,len(words_list)), i)
+        scrambled_words.insert(r.randint(0,len(words_list)), i)
 
     print(scrambled_words)
 
-mi_words()
+mix_words()
