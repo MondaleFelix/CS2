bee40a718d6d5826b5c9c7beb4bd085beab41065
Mondale Felix <mondalefelix@yahoo.com>
2017-10-28 15:24:56 -0700

    cleaned code

--- reverse.py
+++ reverse.py
@@ -3,12 +3,10 @@ import random
 
 string_to_be_reversed = sys.argv[1:]
 
-
-
-
 def reverse_string(string_to_be_reversed):
-    list(string_to_be_reversed)
     string_to_be_reversed.reverse()
+    string_to_be_reversed = " ".join(string_to_be_reversed)
     print(string_to_be_reversed)
+
 
 reverse_string(string_to_be_reversed)
