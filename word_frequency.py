80950d6c7ce192170b6ccfaca8f32f0eb19ddfaa
Mondale Felix <mondalefelix@yahoo.com>
2017-10-31 11:32:11 -0700

    finished algorithm

--- word_frequency.py
+++ word_frequency.py
@@ -5,19 +5,17 @@ with open("fish.txt", "r") as f:
     f_contents = f.read()
     f_contents = f_contents.strip("\n")
     f_contents = f_contents.split(" ")
-    print(f_contents)
 
-    # # for word in f_contents:
-    #     word.replace('\n', '')
+    for word in f_contents:
+        first_word = f_contents[f_contents.index(word)]
+        count = f_contents.count(word)
+        histogram.append([first_word,count])
 
+    for content_list in histogram:
+        count = histogram.count(content_list)
+        if count != 1:
+            histogram.remove(content_list)
+    print(histogram)
 
-    # for word in f_contents:
-    #     first_word = f_contents[f_contents.index(word)]
-    #     count = f_contents.count(word)
-    #     histogram.append([first_word,count])
-    #     if count != 1:
-    #         f_contents.remove(word)
-    #     print(f_contents)
-
-
+    # print(f_contents)
     # print(histogram)
