0ab991bdc6a387d63f387add13734ef0722cd621
Mondale Felix <mondalefelix@yahoo.com>
2017-11-01 16:31:03 -0700

    refactored code

--- word_frequency.py
+++ word_frequency.py
@@ -1,15 +1,17 @@
-histogram = []
-counter = 0
-
-with open("fish.txt", "r") as f:
-    f_contents = f.read()
-    f_contents = f_contents.strip("\n")
-    f_contents = f_contents.split(" ")
-
+def dictionary(f_contents):
+    histogram = {}
     for word in f_contents:
         first_word = f_contents[f_contents.index(word)]
         count = f_contents.count(word)
-        histogram.append([first_word,count])
+        histogram[first_word] = count
+    print(histogram)
+
+def nested_array(f_contents):
+    histogram = []
+    for word in f_contents:
+        first_word = f_contents[f_contents.index(word)]
+        count = f_contents.count(word)
+        histogram.append([first_word, count])
 
     for content_list in histogram:
         count = histogram.count(content_list)
@@ -17,5 +19,28 @@ with open("fish.txt", "r") as f:
             histogram.remove(content_list)
     print(histogram)
 
-    # print(f_contents)
-    # print(histogram)
+def tuple(f_contents):
+    histogram = []
+    for word in f_contents:
+        first_word = f_contents[f_contents.index(word)]
+        count = f_contents.count(word)
+        histogram.append((first_word, count))
+
+    for content_list in histogram:
+        count = histogram.count(content_list)
+        if count != 1:
+            histogram.remove(content_list)
+    print(histogram)
+
+
+def open_file(file_name):
+    with open(file_name, "r") as f:
+        f_contents = f.read()
+        f_contents = f_contents.strip("\n")
+        f_contents = f_contents.split(" ")
+    return f_contents
+
+words_list = open_file("fish.txt")
+tuple(words_list)
+nested_array(words_list)
+dictionary(words_list)
